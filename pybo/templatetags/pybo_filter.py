32d7fd047e1754106de3d8924d6e68ab9f9dabe4
rlarbghrbgh <rlarbghrbgh@gmail.com>
2022-02-19 22:30:26 +0900

    질문, 답변 마크다운 적용 - templatetags -> filter.py -> mark (템플릿에서 사용하는 공통 함수 정도로 현재 이해함...)

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,6 +1,17 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
+
+
+# 마크다운 필터 등록하기
+@register.filter()
+def mark(value):
+    # 확장 도구 설정
+    # nl2br (줄발꿈 문자를 <br> 태그로 치환), fenced_code (소스코드 표현 적용)
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
 
 
 # 템플릿에서 해당 함수를 필터로 사용할 수 있게 됨
